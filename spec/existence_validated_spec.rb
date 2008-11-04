f544dbe9026cd0ccc8c3379e4f48596e1cee5163
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-11-04 13:20:42 +0100

    Added spec_helper and infrastructure

---
+++ spec/existence_validated_spec.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe "existence_validated" do
+  
+  before(:all) do
+    class Model
+      belongs_to :assoc
+      validates_existence_of :assoc
+    end
+  end
+  
+  def valid_attributes(options={})
+    {
+      :name => "Name"
+    }.update(existence_validated([:assoc], options)).update(options)
+  end
+  
+  it "should not fail" do
+    Model.create!(valid_attributes)
+  end
+end
