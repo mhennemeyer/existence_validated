3daa8be1c40a8e0a3bf994cf365a2e1dd9c62b2f
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-11-05 11:55:16 +0100

    Refactored upgly iteration to inject

--- lib/existence_validated.rb
+++ lib/existence_validated.rb
@@ -1,18 +1,16 @@
 def existence_validated(assocs=[], options={})
   assocs = [assocs] unless assocs.respond_to?(:each)
-  hash ||= {}
-  assocs.each do |assoc|
+  assocs.inject({}) do |memo, assoc|
     unless options.has_key?(assoc) && options[assoc] == nil
       klass = assoc.to_s.camelize.constantize
       mock = options[assoc] || mock_model(klass)
       klass.should_receive(:exists?).any_number_of_times.and_return do |fk|
         fk ? true : false
       end
-      hash.update({
+      memo.update({
         assoc.to_s.concat("_id").to_sym => mock.id,
         assoc => mock
       })
-    end
+    end || {}
   end
-  hash
 end
