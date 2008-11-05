09ef258221b0cb2bf255bed7aee8b7f9b846da30
Matthias Hennemeyer <mhennemeyer@gmail.com>
2008-11-05 11:37:11 +0100

    Using should receive with computed return value, replacing multiple calls to it.

--- lib/existence_validated.rb
+++ lib/existence_validated.rb
@@ -5,8 +5,9 @@ def existence_validated(assocs=[], options={})
     unless options.has_key?(assoc) && options[assoc] == nil
       klass = assoc.to_s.camelize.constantize
       mock = options[assoc] || mock_model(klass)
-      klass.should_receive(:exists?).any_number_of_times.with(mock.id).and_return(true)
-      klass.should_receive(:exists?).any_number_of_times.with(nil).and_return(false)
+      klass.should_receive(:exists?).any_number_of_times.and_return do |fk|
+        fk ? true : false
+      end
       hash.update({
         assoc.to_s.concat("_id").to_sym => mock.id,
         assoc => mock
